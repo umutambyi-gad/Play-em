f378a8c3b6adeb6e9117d10027ddcf7f5f114de2
Umutambyi Gad <umutambyig@gmail.com>
2020-11-15 10:42:17 +0100

    Updated UI and added top icon

--- play.py
+++ play.py
@@ -3,28 +3,36 @@ from tkinter import *
 import tkinter.font as tkFont
 
 App = Tk(className="Search")
-App.geometry("600x350")
-App.configure(bg='#5a05f1')
+App.iconbitmap('search.ico')
+App.geometry("570x320")
+App.configure(bg='#241e79')
 
-fontStyle = tkFont.Font(family='Courier', size=14, weight='bold')
+fontStyle = tkFont.Font(family='Courier', size=13, weight='bold')
 
-label = Label(text='Enter any text you think is included in the song name', width=100, height=2, fg='black', bg='#056cf1', font=fontStyle)
+label = Label(text='Enter any text you think is included in the song name', width=100, height=2, fg='black', bg='#673ab7', font=fontStyle)
 label.pack()
 
+# an input field
 inputData = Entry(App, bd=5, width=40, fg='dodgerblue', font=tkFont.Font(family='Lucida Grande', size=12, weight='bold'))
 inputData.pack(side=LEFT, ipady=4)
+inputData.focus()
+inputData.place(x=95, y=100)
 
+# open `AddPathHere.txt` file to read the path(song location)
 with open('AddPathHere.txt') as file:
 	path = file.read().strip()
 
-
+# function for searching and playing songs
 def playSong():
 	try:
 		song = inputData.get().strip()
 		for songs in os.listdir(path):
 			if song in songs.lower():
 				song_dir = os.path.join(path, songs)
-				if os.path.isfile(song_dir) and songs.endswith('mp4'):
+				mp_3 = songs.endswith('mp4')
+				mp_4 = songs.endswith('mp4')
+				jpg = songs.endswith('jpg')
+				if os.path.isfile(song_dir) and mp_3 or mp_4 or jpg:
 					os.startfile(song_dir)
 					inputData.delete(first=0, last=100)
 		label['text'] = 'Enter any text you think is included in the song name'
@@ -34,9 +42,27 @@ def playSong():
 		label['text'] = 'Check the permission or unlock the driver if is locked'
 
 
+# Button for search (submit)
+search_button = Button(App, text="Search", fg='white', bg='#783ab7', activebackground='#197fda', width=10, height=1, bd=4, command=playSong, font=fontStyle)
+search_button.pack(side=RIGHT, ipady=5)
+search_button.place(x=300, y=170)
 
-button = Button(App, text="Search", fg='white', bg='#179fca', activebackground='#197fda', width=10, height=1, bd=4, command=playSong, font=fontStyle)
-button.pack(side=RIGHT, ipady=5)
+# Trigger Enter key to run playSong()
+inputData.bind('<Return>', lambda event: playSong())
 
+# Button for reset
+reset_button = Button(App, text="reset", fg='white', bg='#783ab7', activebackground='lightgreen', width=10, height=1, bd=4, font=fontStyle)
+reset_button.pack()
+reset_button.place(x=150, y=170)
+
+# Button for quit
+quit_button = Button(App, text='Quit', fg='white', bg='#341e79', activebackground='lightgreen', width=11, height=1, bd=4, command=App.destroy, font=fontStyle)
+quit_button.pack(side=BOTTOM)
+quit_button.place(x=220, y=250)
+
+# Trigger Escape<esc> key to destroy App
+App.bind('<Escape>', lambda event: App.destroy())
+
+# disable resizing
 App.resizable(False, False)
 App.mainloop()
